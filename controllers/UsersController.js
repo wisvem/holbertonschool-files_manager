45830a0360e7819a9458c8dd0603af43bab60caf
Wiston Venera Macías <wisvem@hotmail.com>
2022-01-20 23:52:56 +0000

    Change response

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -4,20 +4,17 @@ import dbClient from '../utils/db';
 class UsersController {
   static async postNew(request, response) {
     const { email, password } = request.body;
-    if (!email) return response.status(400).send('Missing email');
-    if (!password) return response.status(400).send('Missing password');
+    if (!email) return response.status(400).send({ error: 'Missing email' });
+    if (!password) return response.status(400).send({ error: 'Missing password' });
     const emailExists = await dbClient.usersCollection.findOne({ email });
-    if (emailExists) return response.status(400).send('Already exist');
+    if (emailExists) return response.status(400).send({ error: 'Already exist' });
     const sha1Password = sha1(password);
     const result = await dbClient.usersCollection.insertOne({
       email,
       password: sha1Password,
     });
-    const user = {
-      id: result.insertedId,
-      email,
-    };
-    return response.send(201, user);
+    const user = { id: result.insertedId, email };
+    return response.status(201).send(user);
   }
 }
 
